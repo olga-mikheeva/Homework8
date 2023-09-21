ffd2b2e91274ac8c5b86147f20b40491f15659c1
Olga <workpost.mkh@yandex.ru>
2023-09-21 22:30:48 +0300

    Task 54

---
+++ Task54/Program.cs
@@ -0,0 +1,75 @@
+﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int Inputnum(string message)
+{
+  Console.Write(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+
+int[,] Create2DArray(int rows, int columns)
+{
+  return new int[rows, columns];
+}
+
+
+void Fill2DArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+void SortInDecrease(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+void Print2DArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int rows = Inputnum("Введите количество строк: ");
+int cols = Inputnum("Введите количество столбцов: ");
+int[,] anyArray = Create2DArray(rows, cols);
+
+Fill2DArray(anyArray);
+Print2DArray(anyArray);
+Console.WriteLine();
+SortInDecrease(anyArray);
+Print2DArray(anyArray);
